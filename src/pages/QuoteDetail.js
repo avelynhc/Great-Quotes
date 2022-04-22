b34a3f2026da02e0ef37101eeb6bd4bed6f0ea8d
avelynhc <avelynhc@gmail.com>
2022-04-22 13:50:54 -0400

    refactored code to have more flexible routing code

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,26 +1,34 @@
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from "./../components/comments/Comments";
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
-  {id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-  {id: 'q2', author: 'Monica', text: 'Learning React is great!'}
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Monica", text: "Learning React is great!" },
 ];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if(!quote) {
+  if (!quote) {
     return <p>No quote found!</p>;
   }
 
   return (
     <section>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </section>
