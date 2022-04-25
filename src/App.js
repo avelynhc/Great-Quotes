38cbe7fab7463e73de99384c69c80ccf1b3eac19
avelynhc <avelynhc@gmail.com>
2022-04-25 15:26:48 -0400

    modified files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import {Route, Switch, Redirect} from 'react-router-dom';
-
-import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail';
-import Layout from './components/layout/Layout';
-import NotFound from './pages/NotFound';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Layout>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/pages/NotFound.js
+++
@@ -1,7 +0,0 @@
-const NotFound = () => {
-    return <div className='centered'>
-        <p>Pate not found!</p>
-    </div>
-}
-
-export default NotFound;
